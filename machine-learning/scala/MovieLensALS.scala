037b877943e9afaa50eb70938c9c2ea5001c1a07
Timothy Chen <tnachen@gmail.com>
2017-03-15 00:17:02 -0700

    Support Spark 2.1.0 and S3 paths

--- machine-learning/scala/MovieLensALS.scala
+++ machine-learning/scala/MovieLensALS.scala
@@ -14,8 +14,9 @@ import org.apache.spark.mllib.recommendation.{ALS, Rating, MatrixFactorizationMo
 object MovieLensALS {
 
   def main(args: Array[String]) {
+    println("Starting to run MovieLensALS")
 
-    Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
+    Logger.getLogger("org.apache.spark").setLevel(Level.INFO)
     Logger.getLogger("org.eclipse.jetty.server").setLevel(Level.OFF)
 
     if (args.length != 2) {
@@ -28,31 +29,121 @@ object MovieLensALS {
 
     val conf = new SparkConf()
       .setAppName("MovieLensALS")
-      .set("spark.executor.memory", "2g")
-    val sc = new SparkContext(conf)
 
+    val sc = new SparkContext(conf)
     // load personal ratings
 
-    val myRatings = loadRatings(args(1))
+    println("Loading ratings...")
+    val myRatings = loadRatings(sc, args(1))
+    println("Finished loading ratings")
     val myRatingsRDD = sc.parallelize(myRatings, 1)
 
     // load ratings and movie titles
 
     val movieLensHomeDir = args(0)
 
-    val ratings = sc.textFile(new File(movieLensHomeDir, "ratings.dat").toString).map { line =>
+    val ratingsPath = movieLensHomeDir + "ratings.dat"
+    println("Reading ratings from " + ratingsPath + "...")
+    val ratings = sc.textFile(ratingsPath).map { line =>
       val fields = line.split("::")
       // format: (timestamp % 10, Rating(userId, movieId, rating))
       (fields(3).toLong % 10, Rating(fields(0).toInt, fields(1).toInt, fields(2).toDouble))
     }
+    println("Finished reading ratings")
 
-    val movies = sc.textFile(new File(movieLensHomeDir, "movies.dat").toString).map { line =>
+    val moviesPath = movieLensHomeDir + "movies.dat"
+    println("Reading movies from " + moviesPath + "...")
+    val movies = sc.textFile(moviesPath).map { line =>
       val fields = line.split("::")
       // format: (movieId, movieName)
       (fields(0).toInt, fields(1))
     }.collect().toMap
+    println("Finished reading movies")
 
-    // your code here
+
+    val numRatings = ratings.count()
+    val numUsers = ratings.map(_._2.user).distinct().count()
+    val numMovies = ratings.map(_._2.product).distinct().count()
+
+    println("Got " + numRatings + " ratings from "
+      + numUsers + " users on " + numMovies + " movies.")
+
+    // split ratings into train (60%), validation (20%), and test (20%) based on the
+    // last digit of the timestamp, add myRatings to train, and cache them
+
+    val numPartitions = 4
+    val training = ratings.filter(x => x._1 < 6)
+      .values
+      .union(myRatingsRDD)
+      .repartition(numPartitions)
+      .cache()
+    val validation = ratings.filter(x => x._1 >= 6 && x._1 < 8)
+      .values
+      .repartition(numPartitions)
+      .cache()
+    val test = ratings.filter(x => x._1 >= 8).values.cache()
+
+    val numTraining = training.count()
+    val numValidation = validation.count()
+    val numTest = test.count()
+
+    println("Training: " + numTraining + ", validation: " + numValidation + ", test: " + numTest)
+
+    // train models and evaluate them on the validation set
+
+    val ranks = List(8, 12)
+    val lambdas = List(0.1, 10.0)
+    val numIters = List(10, 20)
+    var bestModel: Option[MatrixFactorizationModel] = None
+    var bestValidationRmse = Double.MaxValue
+    var bestRank = 0
+    var bestLambda = -1.0
+    var bestNumIter = -1
+    for (rank <- ranks; lambda <- lambdas; numIter <- numIters) {
+      val model = ALS.train(training, rank, numIter, lambda)
+      val validationRmse = computeRmse(model, validation, numValidation)
+      println("RMSE (validation) = " + validationRmse + " for the model trained with rank = "
+        + rank + ", lambda = " + lambda + ", and numIter = " + numIter + ".")
+      if (validationRmse < bestValidationRmse) {
+        bestModel = Some(model)
+        bestValidationRmse = validationRmse
+        bestRank = rank
+        bestLambda = lambda
+        bestNumIter = numIter
+      }
+    }
+
+    // evaluate the best model on the test set
+
+    val testRmse = computeRmse(bestModel.get, test, numTest)
+
+    println("The best model was trained with rank = " + bestRank + " and lambda = " + bestLambda
+      + ", and numIter = " + bestNumIter + ", and its RMSE on the test set is " + testRmse + ".")
+
+    // create a naive baseline and compare it with the best model
+
+    val meanRating = training.union(validation).map(_.rating).mean
+    val baselineRmse =
+      math.sqrt(test.map(x => (meanRating - x.rating) * (meanRating - x.rating)).mean)
+    val improvement = (baselineRmse - testRmse) / baselineRmse * 100
+    println("The best model improves the baseline by " + "%1.2f".format(improvement) + "%.")
+
+    // make personalized recommendations
+
+    val myRatedMovieIds = myRatings.map(_.product).toSet
+    val candidates = sc.parallelize(movies.keys.filter(!myRatedMovieIds.contains(_)).toSeq)
+    val recommendations = bestModel.get
+      .predict(candidates.map((0, _)))
+      .collect()
+      .sortBy(- _.rating)
+      .take(50)
+
+    var i = 1
+    println("Movies recommended for you:")
+    recommendations.foreach { r =>
+      println("%2d".format(i) + ": " + movies(r.product))
+      i += 1
+    }
 
     // clean up
     sc.stop()
@@ -68,16 +159,15 @@ object MovieLensALS {
   }
 
   /** Load ratings from file. */
-  def loadRatings(path: String): Seq[Rating] = {
-    val lines = Source.fromFile(path).getLines()
-    val ratings = lines.map { line =>
+  def loadRatings(sc: SparkContext, path: String): Seq[Rating] = {
+    val ratings = sc.textFile(path).map { line =>
       val fields = line.split("::")
       Rating(fields(0).toInt, fields(1).toInt, fields(2).toDouble)
-    }.filter(_.rating > 0.0)
+    }.filter(_.rating > 0.0).collect()
     if (ratings.isEmpty) {
       sys.error("No ratings provided.")
     } else {
-      ratings.toSeq
+      ratings
     }
   }
 }
--- machine-learning/scala/build.sbt
+++ machine-learning/scala/build.sbt
@@ -6,6 +6,11 @@ name := "movielens-als"
 
 version := "0.1"
 
-scalaVersion := "2.10.4"
+scalaVersion := "2.11.8"
 
-libraryDependencies += "org.apache.spark" % "spark-mllib_2.10" % "1.1.0"
+libraryDependencies += "org.apache.spark" % "spark-mllib_2.11" % "2.1.0"
+
+mergeStrategy in assembly := {
+  case PathList("META-INF", xs @ _*) => MergeStrategy.discard
+  case x => MergeStrategy.first
+}
